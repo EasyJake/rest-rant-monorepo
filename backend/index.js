0287d695b96a6feaad447408b97bd03b5c000ad9
Jacob Daley <easyj4ke@gmail.com>
2024-08-21 12:45:26 -0700

    Building out the backend.

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,71 @@
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Express Settings
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Controllers & Routes
 
-app.use(express.urlencoded({ extended: true }))
+// Root Route - Home Page
+app.get('/', (req, res) => {
+    res.send('Welcome to the REST-Rant API Home Page');
+});
 
-app.use('/places', require('./controllers/places'))
-app.use('/users', require('./controllers/users'))
+// Places Routes
+app.get('/places', (req, res) => {
+    // Logic for listing all places
+    res.send('List of all places');
+});
+
+app.post('/places', (req, res) => {
+    // Logic for creating a new place
+    res.send('New place created');
+});
+
+app.get('/places/:placeId', (req, res) => {
+    // Logic for getting details of a particular place
+    res.send(`Details about place with id ${req.params.placeId}`);
+});
+
+app.put('/places/:placeId', (req, res) => {
+    // Logic for updating a particular place
+    res.send(`Place with id ${req.params.placeId} updated`);
+});
+
+app.delete('/places/:placeId', (req, res) => {
+    // Logic for deleting a particular place
+    res.send(`Place with id ${req.params.placeId} deleted`);
+});
+
+app.post('/places/:placeId/comments', (req, res) => {
+    // Logic for creating a comment about a particular place
+    res.send(`Comment added to place with id ${req.params.placeId}`);
+});
+
+app.delete('/places/:placeId/comments/:commentId', (req, res) => {
+    // Logic for deleting a comment from a particular place
+    res.send(`Comment with id ${req.params.commentId} deleted from place with id ${req.params.placeId}`);
+});
+
+// Users Routes
+app.get('/users', (req, res) => {
+    // Logic for listing all users
+    res.send('List of all users');
+});
+
+app.post('/users', (req, res) => {
+    // Logic for creating a new user (sign-up)
+    res.send('New user created');
+});
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on ${process.env.PORT}`)
-})
+    console.log(`Listening on ${process.env.PORT}`);
+});
