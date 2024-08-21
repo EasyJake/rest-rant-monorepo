1913aadcf0cfad99ff4b4773573fcc45aea47faa
Jacob Daley <easyj4ke@gmail.com>
2024-08-21 10:54:41 -0700

    Setting up project to run locally
    Working through .env, .gitignore, and backend setup

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Express Settings
 app.use(cors())
